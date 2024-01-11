0e2cec9a9b60e1b7b6bd177256ad71c065789aaa
endre88 <endre1988@freemail.hu>
2024-01-11 10:22:22 +0100

    Updates

--- app.js
+++
@@ -1,8 +0,0 @@
-import './index.html';
-import './css/style.css';
-import './css/listBreedsComponent.css';
-import SearchImage from './searchImgaeComponent/searchImageComponent';
-import ListBreeds from './listBreedsComponent/listBreedsComponent';
-
-new SearchImage();
-new ListBreeds();
